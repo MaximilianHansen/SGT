4967d9b30d6956047e331f6c292caa6c34a2b183
Maximilian <maximilian@DESKTOP-UDC3AE6.lan>
2022-01-25 18:21:39 -0500

    second

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -1,33 +1,35 @@
 import launchPhone from '../../img/launchPhone.png'
+import './Phone.css'
 
 
 function Phone() {
     return (
-        <>
-
-        <div style={{width:'85%', display:'flex', alignItems:'flex-start', flexDirection:'column'}}>
-          <h1 style={{marginBottom:'10px', marginTop: '20px'}}>How it works</h1>
-        </div>
-        <div style={{width:'90%', display:'flex', alignItems:'flex-start', flexDirection:'column', marginBottom:'30px'}}>
-          <ul>
-            <li>Groups of 4 people</li>
-            <li>45 Minute Workouts</li>
-            <li>Expertly led instruction</li>
-            <li>Sessions available morning, afternoon, and evening</li>
-          </ul>
-        </div>
         
-
-
-         <div style={{width:'100%', display:'flex', alignItems:'center', flexDirection:'column', backgroundColor:'#f9f9f9'}}>
-          <h1 style={{width:'85%', marginBottom:'15px', marginTop: '35px'}}>Get on a schedule</h1>
+    <div>
+        <div>
+            <div id='titleWrap'>
+              <h1 id='title'>How it works</h1>
+            </div>
+            <div id='listDiv' >
+              <ul>
+                <li>Groups of 4 people</li>
+                <li>45 Minute Workouts</li>
+                <li>Expertly led instruction</li>
+                <li>Sessions available morning, afternoon, and evening</li>
+              </ul>
+            </div>
         </div>
-        <div style={{width:'100%', display:'flex', alignItems:'center', flexDirection:'column', backgroundColor:'#f9f9f9'}}>
-            
-            <img src={launchPhone} style={{width:"100%"}}></img>
+
+        <div>
+            <div id='titleWrap'>
+              <h1 id='title2'>Get on a schedule</h1>
+            </div>
+            <div id='phoneWrap'>
+                <img id='imgPhone'src={launchPhone} style={{width:"100%"}}></img>
+            </div>
         </div>
-        
-        </>
+    </div>
+      
     );
   }
   
